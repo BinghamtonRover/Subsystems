1a6c898e79c8bdad10f6cb84b77b622380ed75ff
Levi Lesches <levilesches@gmail.com>
2023-07-27 04:02:28 -0400

    Updated `BurtLogger`

--- bin/subsystems.dart
+++ bin/subsystems.dart
@@ -2,6 +2,6 @@ import "package:subsystems/subsystems.dart";
 import "package:burt_network/burt_network.dart";
 
 void main() async {
-  Logger.level = LogLevel.debug;
+  BurtLogger.level = LogLevel.debug;
   await collection.init();
 }
