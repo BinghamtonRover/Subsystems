dbd0d4a5294698e09a46cf2a2524a50caf86322b
Levi Lesches <levilesches@gmail.com>
2023-08-01 18:30:38 -0400

    Added `SerialDevice` (#2)

---
+++ bin/serial.dart
@@ -0,0 +1,15 @@
+import "dart:typed_data";
+import "package:subsystems/subsystems.dart";
+import "package:burt_network/logging.dart";
+
+const port = "COM11";
+
+void main() async {
+	final device = SerialDevice(
+		portName: port, 
+		readInterval: const Duration(milliseconds: 100),
+	);
+	device.open();
+	device.stream.listen((data) => logger.info("Received: $data"));
+	device.write(Uint8List.fromList([1, 2, 3]));
+}
