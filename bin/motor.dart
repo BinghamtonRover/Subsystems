6266d89cca029ffc1703d0419da349e1ced42713
Binghamton University Rover Team <rover@binghamton.edu>
2023-11-06 19:56:14 -0500

    CAN code to spin the motor

---
+++ bin/motor.dart
@@ -0,0 +1,13 @@
+import "package:subsystems/subsystems.dart";
+import "package:burt_network/logging.dart";
+
+const speed = [0, 0, 0x10, 0];
+
+void main() async {
+  BurtLogger.level = LogLevel.info;  
+  await collection.init();
+  while (true) {
+    collection.can.can.sendMessage(id: 0x303, data: speed);
+    await Future.delayed(Duration(seconds: 1));
+  }
+}
