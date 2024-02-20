c27e18b24016febfbea131489b5b8529e2822d23
Levi Lesches <levilesches@gmail.com>
2024-02-20 13:53:49 -0500

    Updated ro RoverServer

--- lib/src/server.dart
+++ lib/src/server.dart
@@ -6,23 +6,19 @@ import "package:subsystems/subsystems.dart";
 /// 
 /// This server should collect all commands that come in and forward them to the 
 /// appropriate CAN device. All CAN messages should be forwarded to this server.
-class SubsystemsServer extends ServerSocket {
+class SubsystemsServer extends RoverServer {
 	/// Creates a Subsystems server on the given port.
 	SubsystemsServer({required super.port}) : super(device: Device.SUBSYSTEMS);
-
-  @override
-  Future<void> updateSettings(UpdateSetting settings) async {
-    super.updateSettings(settings);
-    if (settings.status == RoverStatus.RESTART) {
-      await collection.dispose();
-      await Future<void>.delayed(const Duration(seconds: 1));
-      await collection.init();
-    }
-  }
 
 	@override
 	void onMessage(WrappedMessage wrapper) {
     if (collection.serial.sendWrapper(wrapper)) return;
     collection.can.sendWrapper(wrapper);
 	}
+
+  @override
+  Future<void> restart() async {
+    await collection.dispose();
+    await collection.init();
+  }
 }
