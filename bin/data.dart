078b578eec57895dbaaf1ba1b40265ff3165d44f
Levi Lesches <levilesches@gmail.com>
2024-05-09 14:49:26 -0400

    Formatted bin/data.dart

--- bin/data.dart
+++ bin/data.dart
@@ -34,21 +34,27 @@ Future<void> main() async {
   final motor = SwingingIterator(0, pi, 0.01);
   final motor2 = SwingingIterator(0, 2*pi, 0.05);
   while (true) {
-    // final x = ArmData(shoulder: MotorData(angle: pi), elbow: MotorData(angle: pi));
-    // final y = GripperData(lift: MotorData(angle: pi));
-    // server.sendMessage(x);
-    // server.sendMessage(y);
-    // await Future<void>.delayed(const Duration(milliseconds: 100));
-    // continue;
-    
     throttle.moveNext();
     voltage.moveNext();
     current.moveNext();
     motor.moveNext();
     motor2.moveNext();
-    final data = DriveData(left: 1, setLeft: true, right: -1, setRight: true, throttle: throttle.current, setThrottle: true, batteryVoltage: voltage.current, batteryCurrent: current.current); 
+    final data = DriveData(
+      left: 1, 
+      setLeft: true, 
+      right: -1, 
+      setRight: true, 
+      throttle: throttle.current, 
+      setThrottle: true, 
+      batteryVoltage: voltage.current, 
+      batteryCurrent: current.current,
+    ); 
     server.sendMessage(data);
-    final data2 = ArmData(base: MotorData(angle: motor2.current), shoulder: MotorData(angle: motor.current), elbow: MotorData(angle: motor.current));
+    final data2 = ArmData(
+      base: MotorData(angle: motor2.current), 
+      shoulder: MotorData(angle: motor.current), 
+      elbow: MotorData(angle: motor.current),
+    );
     server.sendMessage(data2);
     final data3 = GripperData(lift: MotorData(angle: motor.current));
     server.sendMessage(data3);
