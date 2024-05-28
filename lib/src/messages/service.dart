8c58323cf387bb411541b8a4b778d6bcfdf5b5ec
Paul Ampadu <pgrwe>
2024-05-28 05:20:18 -0400

    accidently switched sendWrapper and sendMessage semantics

--- lib/src/messages/service.dart
+++ lib/src/messages/service.dart
@@ -5,12 +5,12 @@ import "package:burt_network/burt_network.dart";
 /// The Subsystems programs (teensys/arduinos) only understands [Message]s.
 abstract class MessageService extends Service {
   /// Unwraps a [WrappedMessage] and sends it to the device 
-  /// Sends an incoming wrapped message from dashboard or autonomy to the subsystems (teensys)
-  /// Sends Data classes ([DriveData], [ArmData], [GripperData], etc.)
+  /// Takes in data from subsystems (teensys) and sends it to the dashboard/autonomy
+  /// Sends Command classes ([DriveCommand], [ArmCommand], [GripperCommand], etc.)
   void sendWrapper(WrappedMessage wrapper);
 
   /// Wraps a message and sends it using [sendWrapper].
-  /// Takes in data from subsystems (teensys) and sends it to the dashboard/autonomy
-  /// Sends Command classes ([DriveCommand], [ArmCommand], [GripperCommand], etc.)
+  /// Sends an incoming wrapped message from dashboard or autonomy to the subsystems (teensys)
+  /// Sends Data classes ([DriveData], [ArmData], [GripperData], etc.)
   void sendMessage(Message message) => sendWrapper(message.wrap());
 }
